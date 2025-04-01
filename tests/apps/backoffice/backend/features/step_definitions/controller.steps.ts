ac8abf8e846ab9a479dffc46612422f493c363a3
AnderGI <agibarguen@gmail.com>
2025-04-01 19:57:29 +0200

    Upgraded tsc compiler version for build command

--- tests/apps/backoffice/backend/features/step_definitions/controller.steps.ts
+++ tests/apps/backoffice/backend/features/step_definitions/controller.steps.ts
@@ -1,4 +1,4 @@
-import { AfterAll, BeforeAll, Given, Then } from '@cucumber/cucumber';
+import { BeforeAll, Given, Then } from '@cucumber/cucumber';
 import assert from 'assert';
 import { createReadStream, existsSync, mkdirSync, writeFileSync } from 'fs';
 import path, { basename } from 'path';
@@ -34,30 +34,21 @@ Then('the response should be empty', () => {
 });
 
 BeforeAll(() => {
-	const uploadsPath = '/home/andergi/ander/repositories/Gilito/tests/uploads';
+	const uploadsPath = path.resolve(process.cwd(), 'tests/uploads');
 
-	// Crear directorio si no existe
 	mkdirSync(uploadsPath, { recursive: true });
 
-	// Rutas de archivos de prueba
 	const txtPath = path.join(uploadsPath, '01-demo.txt');
 	const jsonPath = path.join(uploadsPath, '01-demo.json');
 
-	// Crear archivo de texto si no existe
 	if (!existsSync(txtPath)) {
 		writeFileSync(txtPath, 'Contenido de prueba para 01-demo.txt');
 	}
 
-	// Crear archivo JSON si no existe
 	if (!existsSync(jsonPath)) {
 		writeFileSync(jsonPath, JSON.stringify({ mensaje: 'Este archivo no debería ser válido' }));
 	}
 
-	// Iniciar la aplicación
 	application = new BackofficeBackendApp();
 	application.start();
-});
-
-AfterAll(() => {
-	application.stop();
 });
