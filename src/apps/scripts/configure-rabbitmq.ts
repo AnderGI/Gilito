0886c96c2d6b90bce044d28c64755ed1b39189ef
AnderGI <agibarguen@gmail.com>
2025-04-07 20:24:14 +0200

    configure rabbitmq script refactored

--- src/apps/scripts/configure-rabbitmq.ts
+++ src/apps/scripts/configure-rabbitmq.ts
@@ -6,31 +6,80 @@ import process from 'process';
 
 const envFile = path.resolve(process.cwd(), `.env.${process.env.NODE_ENV!}`);
 configDotenv({ path: envFile });
+
+class RabbitMqConnectionConfig {
+	public static getConnectionConfig() {
+		return {
+			hostname: process.env.RABBITMQ_HOSTNAME!,
+			username: process.env.RABBITMQ_USERNAME!,
+			password: process.env.RABBITMQ_PASSWORD!,
+			port: parseInt(process.env.RABBITMQ_PORT!, 10)
+		};
+	}
+}
+
+class RabbitMQConnection {
+	private readonly connection!: amqplib.ChannelModel;
+	private readonly channel!: amqplib.ConfirmChannel;
+	public constructor(connection: amqplib.ChannelModel, channel: amqplib.ConfirmChannel) {
+		this.connection = connection;
+		this.channel = channel;
+	}
+
+	public static async create() {
+		const connection = await amqplib.connect(RabbitMqConnectionConfig.getConnectionConfig());
+		const confirmChannel = await connection.createConfirmChannel();
+
+		return new RabbitMQConnection(connection, confirmChannel);
+	}
+
+	public async declareExchanges(
+		exchange: string,
+		type: 'direct' | 'topic' | 'headers' | 'fanout' | 'match' | string,
+		options?: amqplib.Options.AssertExchange
+	) {
+		await this.channel.assertExchange(exchange, type, options);
+	}
+
+	public async close() {
+		await this.closeConfirmChannel();
+		await this.closeConnection();
+	}
+
+	private async closeConfirmChannel() {
+		try {
+			await this.channel.close();
+		} catch {
+			console.error('error closing the confirm channel');
+		}
+	}
+
+	private async closeConnection() {
+		try {
+			await this.connection.close();
+		} catch {
+			console.error('error closing the connection');
+		}
+	}
+}
+
 async function setup() {
-	const connection = await amqplib.connect({
-		hostname: process.env.RABBITMQ_HOSTNAME!,
-		username: process.env.RABBITMQ_USERNAME!,
-		password: process.env.RABBITMQ_PASSWORD!,
-		port: parseInt(process.env.RABBITMQ_PORT!, 10)
-	});
-	const confirmChannel = await connection.createConfirmChannel();
+	const connection = await RabbitMQConnection.create();
 
-	await confirmChannel.assertExchange('domain_events', 'topic', {
-		durable: true,
-		autoDelete: false
-	});
-
-	await confirmChannel.assertExchange('domain_events_retry', 'topic', {
-		durable: true,
-		autoDelete: false
-	});
-
-	await confirmChannel.assertExchange('domain_events_dead_letter', 'topic', {
-		durable: true,
-		autoDelete: false
-	});
-
-	await confirmChannel.close();
+	await Promise.all([
+		connection.declareExchanges('domain_events', 'topic', {
+			durable: true,
+			autoDelete: false
+		}),
+		connection.declareExchanges('domain_events_retry', 'topic', {
+			durable: true,
+			autoDelete: false
+		}),
+		connection.declareExchanges('domain_events_dead_letter', 'topic', {
+			durable: true,
+			autoDelete: false
+		})
+	]);
 
 	await connection.close();
 }
